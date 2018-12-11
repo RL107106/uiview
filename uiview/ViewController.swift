230eded36144895e5b9bf243a9022a33922c7b57
NATHAN GEHRKE <rl107106@rlas-macair-22.local>
2018-12-11 11:47:19 -0600

    Initial Commit

---
+++ uiview/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  uiview
+//
+//  Created by NATHAN GEHRKE on 10/5/18.
+//  Copyright © 2018 Nathan Gehrke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+        var newView = UIView(frame: CGRect(x: 20, y: 50, width: 150, height: 150))
+view.addSubview(newView)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
+}
+}
